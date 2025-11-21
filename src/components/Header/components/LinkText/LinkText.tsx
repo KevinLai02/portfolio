7190fcd74f55a1fab56d75b7966b73f7822e99c4
KevinLai02 <jihn.smith629@gmail.com>
2025-11-21 11:16:34 +0800

    set global font family

--- src/components/Header/components/LinkText/LinkText.tsx
+++ src/components/Header/components/LinkText/LinkText.tsx
@@ -3,7 +3,7 @@ import type { Props } from "./types";
 const LinkText = ({ value, targetId }: Props) => {
 	return (
 		<a
-			className="py-4 px-5 outline-none text-gray-500 hover:text-white font-mono"
+			className="py-4 px-5 outline-none text-gray-500 hover:text-white"
 			href={`#${targetId}`}
 		>
 			{value}
