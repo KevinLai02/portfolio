ac34502f9a1185e15a0056430b251499aa69f462
KevinLai02 <jihn.smith629@gmail.com>
2025-11-22 22:21:24 +0800

    finish project tag feature

--- src/container/Project/components/Tag/Tag.tsx
+++ src/container/Project/components/Tag/Tag.tsx
@@ -1,5 +1,11 @@
-const Tag = () => {
-	return <div>Tag</div>;
+import type { Props } from "./types";
+
+const Tag = ({ value }: Props) => {
+	return (
+		<div className="flex items-center justify-center border-none rounded-md text-white bg-zinc-900 text-sm px-3">
+			{value}
+		</div>
+	);
 };
 
 export default Tag;
--- src/container/Project/constants.ts
+++ src/container/Project/constants.ts
@@ -1,26 +1,46 @@
 export const projectList = [
 	{
-    id: 0,
+		id: 0,
 		title: "Codehelp",
 		description:
-			"A full-stack e-commerce solution with payment integration, inventory management, and admin dashboard.",
+			"A full-stack e-commerce solution with payment integration, inventory management, and admin dashboard.A full-stack e-commerce solution with payment integration, inventory management, and admin dashboard.",
 		projectUrl: "https://codehelp-web.vercel.app/en",
 		imageUrl: "/modern-ecommerce-dashboard.png",
+		tags: [
+			{ id: 0, name: "React" },
+			{ id: 1, name: "Node.js" },
+			{ id: 2, name: "MongoDB" },
+			{ id: 3, name: "TypeScript" },
+			{ id: 4, name: "Next.js" },
+			{ id: 5, name: "TailwindCSS" },
+			{ id: 6, name: "Prisma" },
+		],
 	},
 	{
-    id: 1,
+		id: 1,
 		title: "Onlyface",
 		description:
 			"A full-stack e-commerce solution with payment integration, inventory management, and admin dashboard.",
 		projectUrl: "https://www.onlyface.app/",
 		imageUrl: "/modern-ecommerce-dashboard.png",
+		tags: [
+			{ id: 0, name: "React" },
+			{ id: 1, name: "Node.js" },
+			{ id: 2, name: "MongoDB" },
+			{ id: 3, name: "TypeScript" },
+			{ id: 4, name: "Next.js" },
+		],
 	},
 	{
-    id: 2,
+		id: 2,
 		title: "MentorZoo",
 		description:
 			"A full-stack e-commerce solution with payment integration, inventory management, and admin dashboard.",
 		projectUrl: "https://mentor-zoo.vercel.app/",
 		imageUrl: "/modern-ecommerce-dashboard.png",
+		tags: [
+			{ id: 0, name: "React" },
+			{ id: 1, name: "Node.js" },
+		],
 	},
 ];
