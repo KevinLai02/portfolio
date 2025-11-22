929f7652eb902eeb9cf235b14eb88883000ea63e
KevinLai02 <jihn.smith629@gmail.com>
2025-11-22 10:58:04 +0800

    adjust lobby

--- src/components/Lobby/Lobby.tsx
+++ src/components/Lobby/Lobby.tsx
@@ -1,5 +1,13 @@
+import Subtitle from "./components/Subtitle/Subtitle";
+import Title from "./components/Title/Title";
+
 const Lobby = () => {
-	return <div></div>;
+	return (
+		<div className="bg-black h-screen">
+			<Title value="Building Digital Experiences" />
+			<Subtitle value="Full-stack developer passionate about creating beautiful, functional web applications with modern technologies." />
+		</div>
+	);
 };
 
 export default Lobby;
--- src/components/Lobby/components/Subtitle/Subtitle.tsx
+++ src/components/Lobby/components/Subtitle/Subtitle.tsx
@@ -1,11 +1,7 @@
 import type { Props } from "./types";
 
 const Subtitle = ({ value }: Props) => {
-	return (
-		<div className="text-lg font-bold py-4 px-5 outline-none text-white">
-			{value}
-		</div>
-	);
+	return <div className="text-lg font-bold py-4 px-5 text-white">{value}</div>;
 };
 
 export default Subtitle;
