8e4b20457392ca97d13019534a1f93937309c91b
KevinLai02 <jihn.smith629@gmail.com>
2025-11-22 22:41:46 +0800

    project page RWD

--- src/container/Project/Project.tsx
+++ src/container/Project/Project.tsx
@@ -8,20 +8,22 @@ const Project = () => {
 	return (
 		<div
 			id="project"
-			className="flex flex-col items-center justify-center bg-zinc-950 min-h-screen space-y-8 py-10"
+			className="flex flex-col items-center justify-center bg-zinc-950 min-h-screen space-y-8 pb-10 pt-25"
 		>
-			<Title className="text-5xl" value="Featured Projects" />
-			<div>
-				<Subtitle
-					className="text-xl"
-					value="A selection of recent projects showcasing my skills"
-				/>
-				<Subtitle
-					className="text-xl"
-					value="in web development, design, and problem-solving."
-				/>
+			<div className="px-5">
+				<Title className="text-5xl" value="Featured Projects" />
+				<div>
+					<Subtitle
+						className="text-xl"
+						value="A selection of recent projects showcasing my skills"
+					/>
+					<Subtitle
+						className="text-xl"
+						value="in web development, design, and problem-solving."
+					/>
+				</div>
 			</div>
-			<div className="grid grid-cols-2 gap-8 mt-10">
+			<div className="grid lg:grid-cols-2 md:grid-cols-2 gap-8 mt-10 px-4">
 				{projectList.map((project) => (
 					<ProjectCard
 						key={project.id}
