207cb1753df1191255918e6660ab1faf3c8b2698
KevinLai02 <jihn.smith629@gmail.com>
2025-11-22 17:17:18 +0800

    project components design

--- src/container/Home/Home.tsx
+++ src/container/Home/Home.tsx
@@ -1,11 +1,13 @@
 import Header from "~/components/Header/Header";
 import Lobby from "~/container/Lobby/Lobby";
+import Project from "../Project/Project";
 
 export default function Home() {
 	return (
 		<div className="min-h-screen items-center justify-center bg-zinc-50">
 			<Header />
 			<Lobby />
+			<Project />
 		</div>
 	);
 }
